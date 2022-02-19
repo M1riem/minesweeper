658f92dbab22d766cdc2fc3fd7ef676dfa813353
overc0de <overc0de@mail.ru>
2022-02-19 20:45:21 +0700

    Добавлен скрипт таймера

---
+++ scripts/timer.js
@@ -0,0 +1,31 @@
+//utils
+function div(val, by){
+	return (val - val % by) / by;
+}
+
+function getText(sec){
+	hundreds = div(sec, 100);
+	tens = (div(sec, 10)) % 10;
+	units = sec % 10;
+	return (hundreds +''+ tens +''+ units);
+}
+
+function startTimer()
+{
+	sec = 0;
+	timer = setInterval(tick, 1000);
+}
+
+function stopTimer(){
+	clearInterval(timer);
+}
+
+function tick()
+{
+	sec++;
+	_text = getText(sec);
+	document.getElementById("timer").childNodes[1].nodeValue = _text;			
+	if (sec >= 999) stopTimer();			
+}
+
+
