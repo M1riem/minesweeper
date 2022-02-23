7570cceb94a7f1312318f64f8fe53fddb45fab56
overc0de <overc0de@mail.ru>
2022-02-23 09:25:10 +0700

    Рефакторинг - почистила код в script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,205 +4,217 @@ var context = canvas.getContext('2d');
 
 let countRow;//8,14,20
 let countColumn;//8,18,24	
-//поле игры
-var cells = [];
-//текущий уровень игры
-var level;
 
-function _generationBombs(range, amount){
-	
-	let bombs = [];
+class Field{
+	isClosed;
+	cells;
+	constructor(level){
+		this.isClosed = true;
+		this.width = level.widthField;
+		this.height = level.heightField;
+		this.rows = level.countRow;
+		this.columns = level.countColumn;
+		this.amountBombs = level.amountBombs;	
 		
-	for(let i = 0; i < amount; i++)
-	{ 
-		bombs[i] = getRandomInt(range);
-			for (let j = 0; j < amount; j++){
-				if ((bombs[i] == bombs[j]) && (i!=j))
-				{
-					console.log("Replacement bombs[i] = " + bombs[i] + ", i = "+ i + " ; bombs[j] = " + bombs[j]+ ", j = "+ j);
-					bombs[i] = getRandomInt(range);
-					j = 0;
-					console.log("on " + bombs[i]);
+		this.widthCell = this.width/this.countColumn;
+		this.widthCell = this.height/this.countRow;
+		if (isFiniteSizeCell()) return;
+		
+		this.init(this.rows, this.columns);
+	}
+
+	init(){
+		if(isClosed) {
+			for (let i = 0; i < rows; i++)
+			{
+				for (let j = 0; j < columns; j++){
+					
 				}
 			}
-		_sort(bombs);
+		}
 	}
-	return bombs;
+
+	draw()
+	{
+		if( isClosed )
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
 }
 
 //инициализация поля, переписать initialization()
 function drawGrid(level){
 	
-	let bombs = [];
-	let range = countRow * countColumn;
-	console.log("range = " + range + "; Level amountBombs = " + level.amountBombs);
-	
-	//генерация бомб
-	bombs = _generationBombs(range, level.amountBombs);
-	console.log("Rows : " + countRow + "; Columns:" + countColumn);
-	 
-	for (let i=0; i < countRow; i++)
-	{
-		cells[i] = [];
-		for (let j=0; j < countColumn; j++)
-		{
-			cells[i][j] = new Cell(widthCell*j, heightCell*i, 0);
-			
-			if ((bombs.length!=0) && ((i + (i * (countColumn-1)) + j) == bombs[0] ))
-			{
-				let k = i + (i * (countColumn-1)) + j;
-				console.log("(i(" + i + ")" + "* countRow("+countRow+")) + j("+j+")){ " + k + " } == bombs[0]{ " + bombs[0] +" }");
-				cells[i][j].isBomb = true;
-				cells[i][j].countNeighbors = numberBomb;
-				bombs = bombs.slice(1);										
-			}			
-			
-		}
-	}
-	// 
-	for (let i=0; i < countRow; i++)
-	{
-		for (let j=0; j < countColumn; j++)
-		{			
-			if (cells[i][j].isBomb)
-			{
-				//перенисать cell для хранения такого типа данных
-				//(i-1,j-1); (i-1,j); (i-1,j+1)
-				//(i,j -1) ;  bomb	  (i,j+1)
-				//(i+1,j-1); (i+1,j); (i+1,j+1)
-				if (i > 0){
-					cells[i-1][j].countNeighbors = 
-					(cells[i-1][j].isBomb) ? numberBomb : ++cells[i-1][j].countNeighbors;
-					
-					if (j > 0)
-					{
-						cells[i-1][j-1].countNeighbors = cells[i-1][j-1].isBomb ? numberBomb : ++cells[i-1][j-1].countNeighbors;
-					}	
-					if(j < countColumn-1)
-					{
-						cells[i-1][j+1].countNeighbors = cells[i-1][j+1].isBomb ? numberBomb : ++cells[i-1][j+1].countNeighbors;
-					}
-				}
-				if (i < (countRow-1))
-				{					
-					cells[i+1][j].countNeighbors = cells[i+1][j].isBomb ? 9 : ++cells[i+1][j].countNeighbors;
-					if (j > 0)
-					{
-						cells[i+1][j-1].countNeighbors = cells[i+1][j-1].isBomb ? numberBomb : ++cells[i+1][j-1].countNeighbors;
-					}
-					if(j < countColumn-1)
-					{
-						cells[i+1][j+1].countNeighbors = cells[i+1][j+1].isBomb ? numberBomb : ++cells[i+1][j+1].countNeighbors;
-					}
-				}
-				if (j > 0){
-					cells[i][j-1].countNeighbors = cells[i][j-1].isBomb ? numberBomb : ++cells[i][j-1].countNeighbors;							
-				}
-				if (j < countColumn-1){
-					cells[i][j+1].countNeighbors = cells[i][j+1].isBomb ? numberBomb : ++cells[i][j+1].countNeighbors;	
-				}				
-			}
-		}
-	}
-	
-	
-	//draw empty cells in canvas 
-	for (let i=0; i < countRow; i++){
-		for (let j=0; j < countColumn; j++){
-			cells[i][j].drawClose();												
-		}
-	}	
+	let field = new Field(level);
+	//создание поля
+	cells = addField(cells, level.countRow, level.countColumn, level.amountBombs);
+	// console.log(arr);
+	// logCells(cells);
+
+	//draw cells on canvas 
+	drawFieldClosed(level.countRow, level.countColumn);
 	
 	//log empty cells in console
+	logEmptyCells(level);
+	
+}
+
+function initField(rows, columns, cells){	
+	for (let i = 0; i < rows; i++)
+	{	
+		cells[i] = [];
+		for (let j = 0; j < columns; j++)
+		{
+			cells[i][j] = new Cell(widthCell*j, heightCell*i);
+		}
+	}
+	return cells;
+}
+
+function addNumbers(row, column, cells, maxRow, maxColumn){
+	//перенисать cell для хранения такого типа данных
+	//(i-1,j-1); (i-1,j); (i-1,j+1)
+	//(i,j -1) ;  bomb	  (i,j+1)
+	//(i+1,j-1); (i+1,j); (i+1,j+1)
+		
+	for(let i = row-1; i <= row + 1 ; i++)
+	{
+		if ((i >= 0) && (i < maxRow))
+		{
+			for(let j = column-1; j <= column + 1; j++)
+			{					
+				if ((j >= 0) && (j < maxColumn) && (!cells[i][j].isBomb)){
+					cells[i][j].number++;
+					// console.log("number : cells[" + i + "," + j + "]= "+ cells[i][j].number);
+				}
+			}
+		}
+
+	}
+}
+
+function addField(cells, rows, columns, amountBombs ){	
+	
+	//инициализация поля
+	cells = initField(rows, columns, cells);
+	
+	//генерация бомб
+	let range = rows * columns;
+	console.log("range = " + range + "; amount bombs = " + amountBombs);
+	let bombs = createArrayRandom(range, amountBombs);
+	
+	//заполнение поля
+	cells.forEach(function(row, i){
+		cells[i].forEach(function(cell, j){
+			if ((bombs.length!=0) && ((i * columns + j) == bombs[0] ))
+			{
+				let k = (i * columns + j);
+				console.log("(row(" + i + ")" + "* columns("+columns+")) + column("+j+")){ " + k + " } == bombs[0]{ " + bombs[0] +" }");
+				cell.isBomb = true;
+				cell.number = numberBomb;
+				addNumbers(i, j, cells, rows, columns);
+				bombs = bombs.slice(1);										
+			}		
+		})
+	});
+
+	return cells;
+}
+
+function drawFieldClosed(rows, columns){
+	for (let i=0; i < rows; i++){
+		for (let j=0; j < columns; j++){
+			cells[i][j].drawClose();												
+		}
+	}
+}
+
+//log empty cells in console
+function logEmptyCells(level){
 	let _text = "";
-	for (let i=0; i < countRow; i++){
-		for (let j=0; j < countColumn; j++)			
-			if (cells[i][j].countNeighbors == 0) _text += "(" + i + ", " + j + "); ";						
+	for (let i=0; i < level.countRow; i++){
+		for (let j=0; j < level.countColumn; j++)			
+			if (cells[i][j].number == 0) _text += "(" + i + ", " + j + "); ";						
 		if (_text!="") console.log("Empty : " + _text);
 		_text = "";
 	}	
-	
 }
 
-	
+function logCells(cells){
+	cells.forEach(function(row, i, cells){
+		cells[i].forEach(function(cell, j, cells){
+			console.log("cell.x = " + cell.x + ", cell.y = " + cell.y + ",cell.number = " + cell.number );
+		});	
+	});
+}
+
+
 
 //или переименовать функцию (Same = Empty) или добавить третий коэффециент - number
 //cжедать один проход на бумаге
-function _openNearSameCells(i,j)
+//сделать 
+function _openNearEmptyCells( row, column, maxRow = level.countRow, maxColumn = level.countColumn )
 {
-	//console.log("enter in : " + "(" + i + ", " + j + "); ");
-	if (cells[i][j].isOpen)
+	console.log("enter in : " + "(" + row + ", " + column + ");" );
+	if (cells[row][column].isOpen)
 	{	
-		//console.log("(" + i + "," + j + ") - this open = return");
+		console.log("(" + row + "," + column + ") - this open = return");
 		return;
 	}
 	
-	cells[i][j].drawOpen( );
-	//открытие чисел рядом с пустыми клетками, но не бомб
-	_openNearNumbers(i,j);
-	//console.log("(" + i + "," + j + ") - drawOpen");
+	cells[row][column].drawOpen( );
+	console.log("(" + row + "," + column + ") - drawOpen");
+	//открытие чисел рядом с пустыми клетками
+	_openNearNumbers(row, column);
 	
 	
-	//console.log("(" + i + "," + j + ")" + " top=>");	
-	if ( (i > 0) && (cells[i-1][j].countNeighbors == 0)  ){
-	
-		_openNearSameCells(i-1,j);
-		//console.log("top out of: " + "(" + (i-1) + ", " + j + "); in " + "(" + (i) + ", " + j + ") ");
-	}
-	
-	//console.log("(" + i + "," + j + ")" + " left=>");	
-	if ( (j > 0) && (cells[i][j-1].countNeighbors == 0) ){
-		_openNearSameCells(i,j-1);
-		//console.log("left out of: " + "(" + i + ", " + (j-1) + "); in " + "(" + (i) + ", " + j + ") ");
-	}
-	//console.log("(" + i + "," + j + ")" +" right=>");
-	if ( (j < countColumn-1) && (cells[i][j+1].countNeighbors == 0)  ){
-		_openNearSameCells(i,j+1);
-		//console.log("right out of: " + "(" + i + ", " + (j+1) + "); in " + "(" + (i) + ", " + j + ") ");
-	}
-	
-	//console.log("(" + i + "," + j + ")" + " down=>");
-	if ( (i < countRow-1) && (cells[i+1][j].countNeighbors == 0)){
-		_openNearSameCells(i+1,j);
-		//console.log("down out of: " + "(" + (i+1) + ", " + j + "); in " + "(" + (i) + ", " + j + ") ");
-	}
-	
-	//console.log("( exit: " + i + "," + j + ")" );
+	for(let i = row-1; i <= row + 1 ; i++)
+	{
+		if ((i >= 0) && (i < maxRow))
+		{	
+			for(let j = column-1; j <= column + 1; j++)
+			{	
+				if ( (j >= 0) && (j < maxColumn) && (cells[i][j].number == 0) && 
+					!((j==column)&&(i==row))&&
+					((j==column)||(i==row))
+					)
+				{
+					console.log("(" + i + "," + j + ")" + "=>");	
+					_openNearEmptyCells(i, j);
+					console.log("left out of: " + "(" + i + ", " + j + " )");
+				}
+			}
+			
+		}
+
+	}		
+	console.log("( exit: " + row + "," + column + ")" );
 	return;		
 }
 
-//сделать тестовый прогон
-function _openNearNumbers(i,j)
-{
-	if (i > 0)
+function _openNearNumbers(row, column, maxRow = level.countRow, maxColumn = level.countColumn)
+{	
+	for(let i = row-1; i <= row + 1 ; i++)
 	{
-		if ((!cells[i-1][j].isBomb) && (cells[i-1][j].countNeighbors!=0))
-			cells[i-1][j].drawOpen( ); 
-
-		if ( (j > 0) && (!cells[i-1][j-1].isBomb) && (cells[i-1][j-1].countNeighbors!=0)  )
-			cells[i-1][j-1].drawOpen( ); 
+		if ((i >= 0) && (i < maxRow))
+		{
+			for(let j = column-1; j <= column + 1; j++)
+			{	
 	
-		if( (j < countColumn-1) && (!cells[i-1][j+1].isBomb) && (cells[i-1][j+1].countNeighbors!=0) )
-			cells[i-1][j+1].drawOpen( ); 		
-	}
-	if (i < (countRow-1))
-	{
-		
-		if ( (!cells[i+1][j].isBomb) && (cells[i+1][j].countNeighbors!=0)  )
-			cells[i+1][j].drawOpen( ); 
-		
-		if ( (j > 0) && (!cells[i+1][j-1].isBomb) && (cells[i+1][j-1].countNeighbors!=0)  )
-			cells[i+1][j-1].drawOpen( ); 
-	
-		if( (j < countColumn-1) && (!cells[i+1][j+1].isBomb) && (cells[i+1][j+1].countNeighbors!=0) )
-			cells[i+1][j+1].drawOpen( ); 
-	}
+				if ( (j >= 0) && (j < maxColumn) && (!cells[i][j].isBomb) && (cells[i][j].number!=0) )
+				{	
+					console.log("(" + i + "," + j + ") - _openNearNumbers");
+					cells[i][j].drawOpen();
+				}
+			}
+		}
 
-	if ( (j > 0) && (!cells[i][j-1].isBomb) && (cells[i][j-1].countNeighbors!=0) )
-		cells[i][j-1].drawOpen( ); 
-
-	if( (j < countColumn-1) && (!cells[i][j+1].isBomb) && (cells[i][j+1].countNeighbors!=0) )
-		cells[i][j+1].drawOpen( );									
+	}								
 }
 
 //блокировка контекстного меню для canvas
@@ -230,9 +242,9 @@ function mouseDown( event ){
 						{
 							// if (firstLeftClick == 1) { startTimer(); firstLeftClick++;}
 							console.log("нажата левая кнопка мыши: " + "x = " + mouse.x + ", y = " + mouse.y);
-							switch(cell.countNeighbors){
-								case 0: _openNearSameCells(i,j); break;
-								case numberBomb : _openAllSameCells(numberBomb); break;
+							switch(cell.number){
+								case 0: _openNearEmptyCells(i,j); break;
+								case numberBomb : openAllSameCells(numberBomb); break;
 								default : cell.drawOpen( ); break;								
 							}							
 							break;
@@ -254,49 +266,27 @@ function mouseDown( event ){
 }
 
 //открыть все ячейки c указанным номером (в будующем открыть все бомбы)
-function _openAllSameCells(number)
+function openAllSameCells(number)
 {
-	for (let i=0; i < countRow; i++)
-	{
-		for (let j=0; j < countColumn; j++)
-		{
-			if (i > 0){
-				if(cells[i-1][j].countNeighbors == number) 
-					cells[i-1][j].drawOpen( ); 
-				if ((j > 0) && (cells[i-1][j-1].countNeighbors == number)) 
-					cells[i-1][j-1].drawOpen( ); 
-				if ((j < countColumn-1) && (cells[i-1][j+1].countNeighbors == number)) 
-					cells[i-1][j+1].drawOpen( );				
-			}
-			if (i < (countRow-1)){
-				if(cells[i+1][j].countNeighbors == number) 
-					cells[i+1][j].drawOpen( ); 
-				if ((j > 0) && (cells[i+1][j-1].countNeighbors == number)) 
-					cells[i+1][j-1].drawOpen( ); 
-				if ((j < countColumn-1) && (cells[i+1][j+1].countNeighbors == number)) 
-					cells[i+1][j+1].drawOpen( );				
-			}
-			if ((j > 0) && (cells[i][j-1].countNeighbors == number)){
-				cells[i][j-1].drawOpen( );											
-			}
-			if ((j < countColumn-1) && (cells[i][j+1].countNeighbors == number)){
-				cells[i][j+1].drawOpen( );
-			}		
-		}
-	}
+	cells.forEach(function(row, i){
+		cells[i].forEach(function(cell){
+			if(cell.number == number) 
+				cell.drawOpen();
+		});
+			
+	});
 }
 
-//РїРµСЂРµРїРёСЃР°С‚СЊ С„СѓРЅРєС†РёРѕРЅР°Р» 
-//Р’СЃРїРѕРјРѕРіР°С‚РµР»СЊРЅРѕРµ - РєРѕРѕСЂРґРёРЅР°С‚С‹ РґРІРёР¶СѓС‰РµР№СЃСЏ РјС‹С€Рё
+
 let mouse = {x: 0, y: 0};
 $(document).ready(function(){
-	//РљРѕРѕСЂРґРёРЅР°С‚С‹ РєСѓСЂСЃРѕСЂР° РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ РІСЃРµРіРѕ РґРѕРєСѓРјРµРЅС‚Р°
+	//Координаты курсора относительно всего документа
 	$(document).mousemove(function(event){
 		var x = event.pageX;
 		var y = event.pageY;
-		$('#coordsdocument').html( 'РљРѕРѕСЂРґРёРЅР°С‚С‹ РєСѓСЂСЃРѕСЂР°: (' + x + '; ' + y + ')' );
+		$('#coordsdocument').html( 'Координаты курсора: (' + x + '; ' + y + ')' );
 	});
-	// РљРѕРѕСЂРґРёРЅР°С‚С‹ РєСѓСЂСЃРѕСЂР° РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ РѕС‚РґРµР»СЊРЅРѕРіРѕ Р±Р»РѕРєР°
+	// Координаты курсора относительно отдельного блока
 	$(canvas).mousemove(function(event){
 		var pos = $(this).offset();
 		var elem_left = pos.left.toFixed(0);
@@ -304,6 +294,15 @@ $(document).ready(function(){
 		var x = event.pageX - elem_left;
 		var y = event.pageY - elem_top;
 		mouse.x = x; mouse.y = y;
-		$('#coordscanvas').html( 'РљРѕРѕСЂРґРёРЅР°С‚С‹ РєСѓСЂСЃРѕСЂР°-С…РѕР»СЃС‚Р°: (' + x + '; ' + y + ')' );
+		$('#coordscanvas').html( 'Координаты курсора-холста: (' + x + '; ' + y + ')' );
 	});
 });
+
+
+function test(){
+	for(let k = 0; k <= 2; k++){
+		for(let t = 0; t <=2; t++) {
+			if ((k==1)||(t==1)) console.log("(" + k + "," + t + ")");
+		}
+	}
+}
